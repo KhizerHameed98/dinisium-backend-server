75dfd9e5fa2863518bf6b75ab0fbbe64afa294cd
Khizer Hameed <khizerhameed@Khizers-MacBook-Pro.local>
2022-03-11 19:29:31 +0500

    changes

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
-const app = require("./app.js");
-const config = require("./config/configBasic");
-const http = require("http");
-const process = require("process");
+const app = require('./app.js');
+const config = require('./config/configBasic');
+const http = require('http');
+const process = require('process');
 
-const port = config.port;
+// const Port = process.env.SERVER_PORT || 8080;
 
-app.set("port", port);
-const httpServer = http.createServer(app);
+// app.set('port', Port);
+// const httpServer = http.createServer(app);
 
-let server = httpServer.listen(port, () => {
-  console.log("Server running on port ", port);
+// let server = httpServer.listen(Port, () => {
+//   console.log('Server running on port ', Port);
+// });
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log(`express is connected with ${PORT}`);
 });
-process.on("SIGINT", function () {
-  console.log("\nGracefully shutting down from SIGINT (Ctrl-C)");
+
+process.on('SIGINT', function () {
+  console.log('\nGracefully shutting down from SIGINT (Ctrl-C)');
   //  some other closing procedures go here
   process.exit(0);
 });
